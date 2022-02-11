17e9cc7d8aa2fd2c0cafea3049ef2320fae42b4d
Hilson <82933261+Hilsonxhero@users.noreply.github.com>
2022-02-11 13:01:25 +0330

    initial app

---
+++ resources/js/app.vue
@@ -0,0 +1,19 @@
+<template>
+    <router-view></router-view>
+    <!-- <div>
+        <h1>hello</h1>
+    </div> -->
+</template>
+
+<script>
+export default {
+    components: {},
+    data() {
+        return {};
+    },
+    methods: {},
+    mounted() {},
+};
+</script>
+
+<style scoped></style>
---
+++ resources/js/components/HelloWord.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="grid items-center h-screen">
+        <div>
+            <button
+                class="bg-gray-400 py-4 px-6 rounded-lg text-white flex"
+                @click="increment"
+            >
+                {{ state.count }}
+            </button>
+        </div>
+    </div>
+</template>
+<script setup>
+import { ref, reactive } from "vue";
+
+const state = reactive({ count: 0 });
+
+const increment = () => {
+    state.count++;
+};
+</script>
+<style></style>
